9e398722cd35834429ec345f527b2db869d7c300
Roman <pamaxa-s3@ukr.net>
2019-11-16 18:37:54 +0200

    final

--- js/main.js
+++ js/main.js
@@ -4,9 +4,11 @@ $(function () {
   $('.slider__inner').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
-    arrows: false,
+    arrows: true,
     fade: false,
-    asNavFor: '.slider__nav'
+    asNavFor: '.slider__nav',
+    prevArrow: '<button class="slick-arrow slick-prev"><img src="images/slider/arow-prev.svg" alt=""></button>',
+    nextArrow: '<button class="slick-arrow slick-next"><img src="images/slider/arow-next.svg" alt=""></button>',
   });
 
   $('.slider__nav').slick({
@@ -17,6 +19,14 @@ $(function () {
     focusOnSelect: true,
     variableWidth: true,
   });
-    
+
+
+  $('.works__slider-inner').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    variableWidth: true,
+    focusOnSelect: true,
+  });
+
 
 });
