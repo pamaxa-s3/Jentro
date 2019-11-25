5e838c14907d61f26a2d38fd9b3ae6e95333b6bd
Roman <pamaxa-s3@ukr.net>
2019-11-25 08:18:57 +0200

    customer edits

--- js/main.js
+++ js/main.js
@@ -5,12 +5,20 @@ $(function () {
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: true,
+    autoplay: false,
     fade: false,
     asNavFor: '.slider__nav',
     prevArrow: '<button class="slick-arrow slick-prev"><img src="images/slider/arow-prev.svg" alt=""></button>',
     nextArrow: '<button class="slick-arrow slick-next"><img src="images/slider/arow-next.svg" alt=""></button>',
-  
-    
+
+    responsive: [{
+      breakpoint: 767,
+      settings: {
+        arrows: false,
+        autoplay: true
+      }
+    }]
+
   });
 
   $('.slider__nav').slick({
@@ -22,46 +30,23 @@ $(function () {
     variableWidth: true,
     responsive: [{
       breakpoint: 901,
-        settings: {
-          slidesToShow: 5,
-          variableWidth: false,
-        }
-    },
-    {
-      breakpoint: 741,
-        settings: {
-          slidesToShow: 4,
-          variableWidth: false,
-        }
-    },
-    {
-      breakpoint: 607,
-        settings: {
-          slidesToShow: 3,
-          variableWidth: false,
-        }
-    },
-
-    {
-      breakpoint: 456,
-        settings: {
-          slidesToShow: 2,
-          variableWidth: false,
-        }
-    },
-  ]
+      settings: {
+        slidesToShow: 5,
+        variableWidth: false,
+      }
+    }, ]
   });
 
 
-  $('.header__menu ul li').on('click', function(){
+  $('.header__menu ul li').on('click', function () {
     $(this).addClass('active').siblings().removeClass('active')
- });
-
-
-  $('.burger').on('click', function(){
-     $('.header__menu ul').slideToggle();
-     $('.burger').toggleClass('burger_active');
   });
 
-  
+
+  $('.header__menu-btn').on('click', function () {
+    $('.header__menu ul').slideToggle();
+    $('.header__menu-btn').toggleClass('burger_active');
+  });
+
+
 });
